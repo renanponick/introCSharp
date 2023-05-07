8f5440b65c9f01aef506d12ee4d4545c3ac33ed3
Renan <ponickrenan@gmail.com>
2023-05-07 13:52:38 -0300

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace introCSharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Somando.Somar();
+            //Console.WriteLine(Somando.Somar());
+        }
+    }
+}
+
---
+++ Somando.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace introCSharp
+{
+    public class Somando
+    {
+        public static void Somar()
+        {
+            Console.WriteLine("Somando dois números: ");
+            int num1, num2;
+            Console.WriteLine("Infome o primeiro número");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Infome o segundo número");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+
+            // return num1 + num2;
+        }
+    }
+}
