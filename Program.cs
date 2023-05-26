a57114779606b97f3f5b3d363ba8b2119857c139
Renan <ponickrenan@gmail.com>
2023-05-26 16:57:24 -0300

    Feat: add classe pessoa

--- Program.cs
+++ Program.cs
@@ -6,9 +6,45 @@ namespace introCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Somando.Somar();
-            //Console.WriteLine(Somando.Somar());
+            Console.WriteLine("Criando nosso primeiro objeto: ");
+            Pessoa pessoa1 = new Pessoa{
+                Nome = "João Pedro",
+                Idade = 20,
+                Email = "joaozinhoreidelas@hotmail.com",
+                Endereco = "João Costa",
+                Telefone = "(47) 99999-9999",
+            };
+            Console.WriteLine("Informações:\n" + pessoa1.ToString());
+            // Pessoa pessoa2 = new Pessoa{};
+            // pessoa2.Nome = "João Pedro";
+            // pessoa2.Idade = 20;
+            // pessoa2.Email = "joaozinhoreidelas@hotmail.com";
+            // pessoa2.Endereco = "João Costa";
+            // pessoa2.Telefone = "(47) 99999-9999";
+            // Console.WriteLine("Informações:\n" + pessoa2.ToString());
+
+            int numeroPessoas = 5;
+            Pessoa[] pessoas = new Pessoa[numeroPessoas];
+            
+            decimal mediaIdade = 0;
+            // fazer o calculo da media de idade
+            for (int i = 0; i < numeroPessoas; i++) {
+                mediaIdade += Convert.ToDecimal(pessoas[i]);
+            }
+            mediaIdade = mediaIdade / numeroPessoas;
+            Console.WriteLine("A media de idade é: " + mediaIdade);
+
+            // Buscando pessoa
+
+            Console.WriteLine("Digite o nome da pessoa para buscar: ");
+            string nome = Console.ReadLine();
+            for (int i = 0; i < numeroPessoas; i++)
+            {
+                if (pessoas[i].Nome == nome)
+                {
+                    Console.WriteLine(pessoas[i]);
+                }
+            }
         }
     }
 }
