105f38e6779663ec6c247c2559cd37f181631d26
Renan <ponickrenan@gmail.com>
2023-05-28 18:28:30 -0300

    list use

--- Program.cs
+++ Program.cs
@@ -1,74 +1,110 @@
 ﻿using System;
-using System.Windows.Forms;
+using System.Collections.Generic;
 
-namespace LoginApp
+class Program
 {
-    static class Program
+    static List<string> itemList = new List<string>();
+
+    static void Main(string[] args)
     {
-        [STAThread]
-        static void Main()
+        bool exit = false;
+
+        while (!exit)
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
-        }
-    }
+            Console.WriteLine("Selecione uma operação:");
+            Console.WriteLine("1 - Criar item");
+            Console.WriteLine("2 - Ler itens");
+            Console.WriteLine("3 - Atualizar item");
+            Console.WriteLine("4 - Deletar item");
+            Console.WriteLine("5 - Sair");
 
-    public class LoginForm : Form
-    {
-        private Label lblUsername;
-        private Label lblPassword;
-        private TextBox txtUsername;
-        private TextBox txtPassword;
-        private Button btnLogin;
-
-        public LoginForm()
-        {
-            lblUsername = new Label();
-            lblUsername.Text = "Username:";
-            lblUsername.Location = new System.Drawing.Point(20, 20);
-
-            txtUsername = new TextBox();
-            txtUsername.Location = new System.Drawing.Point(100, 20);
-
-            lblPassword = new Label();
-            lblPassword.Text = "Password:";
-            lblPassword.Location = new System.Drawing.Point(20, 50);
-
-            txtPassword = new TextBox();
-            txtPassword.Location = new System.Drawing.Point(100, 50);
-            txtPassword.PasswordChar = '*';
-
-            btnLogin = new Button();
-            btnLogin.Text = "Login";
-            btnLogin.Location = new System.Drawing.Point(100, 80);
-            btnLogin.Click += BtnLogin_Click;
-
-            Controls.Add(lblUsername);
-            Controls.Add(txtUsername);
-            Controls.Add(lblPassword);
-            Controls.Add(txtPassword);
-            Controls.Add(btnLogin);
-
-            Size = new System.Drawing.Size(300, 150);
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "Login";
-        }
-
-        private void BtnLogin_Click(object sender, EventArgs e)
-        {
-            string username = txtUsername.Text;
-            string password = txtPassword.Text;
-
-            if (username == "admin" && password == "12345")
+            int choice;
+            if (int.TryParse(Console.ReadLine(), out choice))
             {
-                MessageBox.Show("Login successful!");
+                switch (choice)
+                {
+                    case 1:
+                        CreateItem();
+                        break;
+                    case 2:
+                        ReadItems();
+                        break;
+                    case 3:
+                        UpdateItem();
+                        break;
+                    case 4:
+                        DeleteItem();
+                        break;
+                    case 5:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
             }
             else
             {
-                MessageBox.Show("Incorrect username or password. Please try again.");
+                Console.WriteLine("Opção inválida.");
             }
+
+            Console.WriteLine();
+        }
+    }
+
+    static void CreateItem()
+    {
+        Console.WriteLine("Digite o item a ser criado:");
+        string item = Console.ReadLine();
+        itemList.Add(item);
+        Console.WriteLine("Item criado com sucesso!");
+    }
+
+    static void ReadItems()
+    {
+        if (itemList.Count > 0)
+        {
+            Console.WriteLine("Itens na lista:");
+            foreach (string item in itemList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        else
+        {
+            Console.WriteLine("A lista está vazia.");
+        }
+    }
+
+    static void UpdateItem()
+    {
+        Console.WriteLine("Digite o índice do item a ser atualizado:");
+        int index;
+        if (int.TryParse(Console.ReadLine(), out index) && index >= 0 && index < itemList.Count)
+        {
+            Console.WriteLine("Digite o novo valor do item:");
+            string newItem = Console.ReadLine();
+            itemList[index] = newItem;
+            Console.WriteLine("Item atualizado com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Índice inválido.");
+        }
+    }
+
+    static void DeleteItem()
+    {
+        Console.WriteLine("Digite o índice do item a ser deletado:");
+        int index;
+        if (int.TryParse(Console.ReadLine(), out index) && index >= 0 && index < itemList.Count)
+        {
+            itemList.RemoveAt(index);
+            Console.WriteLine("Item deletado com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Índice inválido.");
         }
     }
 }
